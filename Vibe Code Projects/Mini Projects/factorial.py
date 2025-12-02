40270c4ed1a6c58ea862d9b534198e1add2611a8
Akshar <akshar.kakkad@gmail.com>
2025-12-01 21:37:34 -0500

    Updated Input

--- Vibe Code Projects/Mini Projects/factorial.py
+++ Vibe Code Projects/Mini Projects/factorial.py
@@ -2,5 +2,5 @@ def factorial(n):
     if n==0 or n==1:
         return 1
     return n*factorial(n-1)
-num=int(input())
+num=int(input("Enter a number to find its factorial: "))
 print(factorial(num))
