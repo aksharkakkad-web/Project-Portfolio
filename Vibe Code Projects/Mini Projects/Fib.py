dfd8a8a47c1317a97de9a381ba670f917cc6bfe0
Akshar <akshar.kakkad@gmail.com>
2025-12-01 19:47:55 -0500

    updated input

--- Vibe Code Projects/Mini Projects/Fib.py
+++ Vibe Code Projects/Mini Projects/Fib.py
@@ -3,5 +3,5 @@ def fib(n):
     for i in range(2,n):
         seq.append(seq[i-1]+seq[i-2])
     print(seq[:n])
-n=input()
+n=input(f"Enter the number of terms in Fibonacci sequence: ")
 print(fib(int(n)))
